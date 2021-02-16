4849080cad6260340f1bc06b57ad4bf0f7ed2f9d
Robert Lange <robert.lange@barcelonagse.eu>
2021-02-16 10:58:33 +0100

    Upadte version numbers

--- setup.py
+++ setup.py
@@ -28,14 +28,14 @@ requires = [
 
 setup(
      name='mle_toolbox',
-     version='0.2.3',
+     version='0.2.4',
      author="Robert Tjarko Lange",
      author_email="robertlange0@gmail.com",
      description="Machine Learning Experiment Toolbox",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/RobertTLange/mle-toolbox",
-     download_url="https://github.com/RobertTLange/mle-toolbox/archive/v0.2.3.tar.gz",
+     download_url="https://github.com/RobertTLange/mle-toolbox/archive/v0.2.4.tar.gz",
      classifiers=[
          "Programming Language :: Python :: 3.6",
          "Programming Language :: Python :: 3.7",
