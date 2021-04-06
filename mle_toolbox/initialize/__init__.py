2ee18c3fa9e3f9cc55cbc54c08656b29bb812966
Robert Lange <robert.lange@barcelonagse.eu>
2021-04-06 13:22:21 +0200

    Refactor `initialize` subdir

---
+++ mle_toolbox/initialize/__init__.py
@@ -0,0 +1,8 @@
+from .config_default import default_mle_config
+from .config_description import description_mle_config
+
+
+__all__ = [
+           "default_mle_config",
+           "description_mle_config",
+          ]
