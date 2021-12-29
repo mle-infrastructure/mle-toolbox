e584f6f4a2cbfde4b643ae1db33ec6c94ccfe2ba
RobertTLange <robertlange0@gmail.com>
2021-12-29 18:52:56 +0100

    Clean up old PBT

--- examples/pbt_quadratic/run_quadratic_steps.py
+++ examples/pbt_quadratic/run_quadratic_steps.py
@@ -2,6 +2,31 @@ import numpy as np
 from mle_toolbox import MLExperiment
 
 
+class QuadraticProblem(object):
+    def __init__(self, theta=None, lrate: float = 0.1):
+        self.lrate = lrate
+
+    def step(self, h, theta):
+        """Perform GradAscent step on quadratic surrogate objective (max!)."""
+        surrogate_grad = -2.0 * h * theta
+        return theta + self.lrate * surrogate_grad
+
+    def evaluate(self, theta):
+        """Ground truth objective (e.g. val loss) - Jaderberg et al. 2016."""
+        return 1.2 - np.sum(theta ** 2)
+
+    def surrogate_objective(self, h, theta):
+        """Surrogate objective (with hyperparams h) - Jaderberg et al. 2016."""
+        return 1.2 - np.sum(h * theta ** 2)
+
+    def __call__(self, theta, hyperparams):
+        h = np.array([hyperparams["h0"], hyperparams["h1"]])
+        theta = self.step(h, theta)
+        exact = self.evaluate(theta)
+        # surrogate = self.surrogate_objective(h, theta)
+        return theta.tolist(), exact
+
+
 def main(mle):
     """Optimize a simple quadratic."""
     # Reload the 'weights' (theta) corresponding to quadratic
@@ -10,42 +35,19 @@ def main(mle):
     else:
         theta = np.array(mle.train_config.theta_init[mle.train_config.worker_id])
 
-    # Combine hyperparameters h0, h1 into np array
-    h = np.stack([mle.train_config.h0, mle.train_config.h1])
+    problem = QuadraticProblem()
 
     for update_counter in range(mle.train_config.num_steps_until_ready):
-        theta = step(theta, h, mle.train_config.lrate)
+        theta, score = problem(theta, mle.train_config)
         # Update log with latest evaluation results
         time_tick = {"num_updates": update_counter + 1}
         stats_tick = {
-            "objective": evaluate(theta),
-            "surrogate": surrogate_objective(theta, h),
+            "score": score,
             "theta_0": theta[0],
             "theta_1": theta[1],
         }
         mle.update_log(time_tick, stats_tick, model=theta, save=True)
-
-    # Stop training if number of steps is 'ready' number reached!
-    if (update_counter + 1) == mle.train_config.num_steps_until_ready:
-        return
-
     return
-
-
-def step(theta, h, lrate):
-    """Perform GradAscent step on quadratic surrogate objective (maximize!)."""
-    surrogate_grad = -2.0 * h * theta
-    return theta + lrate * surrogate_grad
-
-
-def evaluate(theta):
-    """Ground truth objective (e.g. val loss) as in Jaderberg et al. 2016."""
-    return 1.2 - np.sum(theta ** 2)
-
-
-def surrogate_objective(theta, h):
-    """Surrogate objective (with hyperparams h) as in Jaderberg et al. 2016."""
-    return 1.2 - np.sum(h * theta ** 2)
 
 
 if __name__ == "__main__":
